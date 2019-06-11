301b77b93e20b0e0b5e4eede14147f9f8927bae5
pduleba <pduleba@pgs-soft.com>
2019-06-11 12:38:27 +0200

    App-rest :: Jenkinsfile introduction

--- util-jenkins/blueocean-jenkins/exec-run.cmd
+++ util-jenkins/blueocean-jenkins/exec-run.cmd
@@ -3,8 +3,8 @@ docker run ^
   -d ^
   -p 8181:8080 ^
   -p 50000:50000 ^
-  -v D:/docker/jenkins_home:/var/jenkins_home ^
-  -v D:/docker/home:/home ^
+  -v /D/docker/jenkins/jenkins_home:/var/jenkins_home ^
+  -v /D/docker/jenkins/home:/home ^
   -v /var/run/docker.sock:/var/run/docker.sock ^
   --name blueocean-jenkins ^
   pduleba/blueocean-jenkins:latest
