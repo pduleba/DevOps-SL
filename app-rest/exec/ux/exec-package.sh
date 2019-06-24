7f691bf2f3f26b31d5ede8c8b92165ea79df3f02
pduleba <pduleba@pgs-soft.com>
2019-06-24 11:37:38 +0200

    App-rest :: Bug fix

--- app-rest/exec/ux/exec-package.sh
+++ app-rest/exec/ux/exec-package.sh
@@ -5,5 +5,5 @@ cp -r target/$1 target/zip
 cp -r code-deploy/appspec.yml target/zip
 cp -r code-deploy/exec target/zip
 sed -i "s/RELEASE_VERSION/$3/g" target/zip/exec/service_after_install.sh
-cd target/zip && zip -r ../../$2 *
-cd ../..
+cd target/zip && zip -r ../$2 *
+cd ..
