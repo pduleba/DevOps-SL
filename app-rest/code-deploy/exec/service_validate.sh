0f1587e9d2314154d06827d09900030fed6ecc20
pduleba <pduleba@pgs-soft.com>
2019-06-25 15:04:48 +0200

    app-rest :: update validation script

--- app-rest/code-deploy/exec/service_validate.sh
+++ app-rest/code-deploy/exec/service_validate.sh
@@ -2,12 +2,13 @@
 
 while [[ true ]]
 do
-    if [[ $(systemctl is-active app-rest) == 'active' ]]
+    status=$(systemctl is-active app-rest)
+    if [[ $status == 'active' ]]
     then
         echo "Service is running!"
         exit 0
     else
-        echo "Waiting for service..."
+        echo "Waiting for service under $status status ..."
         sleep 3s
     fi
 done
