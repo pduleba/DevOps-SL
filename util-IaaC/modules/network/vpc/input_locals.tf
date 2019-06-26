ebf7521fbc180986e173c6f179d57d4422fe1446
pduleba <pduleba@pgs-soft.com>
2019-06-26 11:17:17 +0200

    IaaC :: Bug fix

--- util-IaaC/modules/network/vpc/input_locals.tf
+++ util-IaaC/modules/network/vpc/input_locals.tf
@@ -6,5 +6,5 @@ locals {
   PUBLIC_SUBNET_COUNT = var.az_use_count
   PRIVATE_SUBNET_COUNT = var.az_use_count
   // public & private subnet per AZ
-  SUBNET_COUNT = PUBLIC_SUBNET_COUNT + PRIVATE_SUBNET_COUNT
+  SUBNET_COUNT = local.PUBLIC_SUBNET_COUNT + local.PRIVATE_SUBNET_COUNT
 }
